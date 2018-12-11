b72fb3ed72a49d5eb234bbf9d285657d017d4062
CarsonRey <c.rey494@gmail.com>
2018-12-10 20:51:26 -0500

    Done.

--- src/Matrix.js
+++ src/Matrix.js
@@ -7,10 +7,24 @@ export default class Matrix extends Component {
 
   constructor() {
     super()
+    this.state = {
+      color: 'fff'
+    }
   }
 
+  childCallBack = (childData) => {
+    this.setState({
+      color: childData
+    })
+  }
+
+  currentColor = () => {
+    return this.state.color
+  }
+
+
   genRow = (vals) => (
-    vals.map((val, idx) => <Cell key={idx} color={val} />)
+    vals.map((val, idx) => <Cell key={idx} color={val} currentColor={this.currentColor}/>)
   )
 
   genMatrix = () => (
@@ -18,10 +32,11 @@ export default class Matrix extends Component {
   )
 
 
+
   render() {
     return (
       <div id="app">
-        <ColorSelector />
+        <ColorSelector callBackFromParent={this.childCallBack}/>
         <div id="matrix">
           {this.genMatrix()}
         </div>
